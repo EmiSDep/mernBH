23f8fd1af91f38d2a2d0da332cdb901facb2ee3c
Emilio Depena <emiliosdep@gmail.com>
2020-07-08 22:55:51 -0400

    post model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
 const isEmpty = require("../../utils/isEmpty");
-const config = require('config')
+const config = require("../../config");
 
 const User = require("../../models/User");
 
@@ -28,14 +28,13 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     const { email, password } = req.body;
-    const userData = { email, password };
+    const userData = { email: email.toLowerCase(), password };
 
     try {
       const salt = await bcrypt.genSalt(10);
       userData.password = await bcrypt.hash(userData.password, salt);
 
       // create the code that will check if the email is already in use before creating the new user.
-
       const existing = await User.findOne({ email: userData.email });
 
       if (!isEmpty(existing)) {
@@ -69,7 +68,7 @@ router.put(
     }
 
     try {
-      const user = await User.findOne({ email: req.body.email });
+      const user = await User.findOne({ email: req.body.email.toLowerCase() });
 
       if (isEmpty(user)) {
         return req.status(404).json({ email: "email not found" });
@@ -83,28 +82,22 @@ router.put(
 
       // validated! challenge create the token and return it to the user.
 
-      // information => encoder + key => token => decoder + key => information.
-      //                                tofe      tobe
-
-      User.findOneByIdAndUpdate(user.id, {lastLogin: Date.now()});
+      User.findByIdAndUpdate(user.id, { lastLogin: Date.now() });
 
       const payload = {
-        id: user._id,
-        email: user.email
+        id: user.id,
+        email: user.email,
         //iat: Date
-      }
+      };
 
-      const token = jwt.sign(payload, config.secretOrKey, {})
+      const token = jwt.sign(payload, config.secretOrKey, {});
 
       return res.json(token);
-
     } catch (error) {
       console.error(error);
       res.status(500).json(error);
     }
   }
 );
-
-router.put();
 
 module.exports = router;
